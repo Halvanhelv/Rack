7e8060edbd8d012c8fcd3788b82fe14e8c3d7217
SynthesisOne <developerfromrussia@gmail.com>
2020-05-30 17:17:35 +0300

    finish

--- time_format.rb
+++ time_format.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 class TimeFormat
-  attr_reader :invalid_params,:result
-  FORMAT = { year: 'year', month: 'month', day: 'day', hour: 'hour', min: 'minute', second: 'sec' }.freeze
+  FORMAT = { year: '%Y', month: '%m', day: '%d', hour: ' %H:', minute: '%M', second: '%S' }.freeze
   def initialize(params)
     @params = params.split(',')
 
   end
 
+  def invalid_params
+    @invalid_params = @params - FORMAT.keys.map(&:to_s)
+  end
+
+  def result
+    Time.now.strftime(call) + "\n"
+  end
 
   def call
-    @invalid_params = @params - FORMAT.keys.map(&:to_s)
+    invalid_params
     @result = []
-    @params.each { |e| result << Time.now.send(FORMAT[e.to_sym])  }
+    @params.each { |e| @result << FORMAT[e.to_sym] }
     @result.join('-')
   end
 
